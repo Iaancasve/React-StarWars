abf1ac904340888a00d9668acee103c031a117f0
Ian <iaancastellanos1234@gmail.com>
2026-01-14 09:34:43 +0100

    Implementacion useState

--- react-starwars/src/StarWarsApp.tsx
+++ react-starwars/src/StarWarsApp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ImageList } from "./gifs/components/ImageList.tsx";
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { mockImage } from "./mock-data/images.mock.ts";
@@ -5,6 +6,13 @@ import { CustomHeader } from "./shared/components/CustomHeader";
 import { SearchBar } from "./shared/components/SearchBar";
 
 export const StarWarsApp = () => {
+
+  const [previousTerms, setPreviousTerms] = useState(['Darth Vader']);
+
+  const handleTermClicked = (term: string) => {
+    console.log(`Term clicked: ${term}`);
+  }
+  
   return (
     <>
       {/* Header */}
@@ -17,7 +25,7 @@ export const StarWarsApp = () => {
       <SearchBar placeholder="Busca lo que quieras"/>
 
       {/* Búsquedas previas*/}
-      <PreviousSearches searches={['Luke', 'Darth Vader']} />
+      <PreviousSearches searches={previousTerms} onLabelClicked={handleTermClicked}/>
 
       {/* Imagenes */}
       <ImageList images={mockImage}/>
